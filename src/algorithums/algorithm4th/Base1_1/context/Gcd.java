7a5b4c212db747b25f5390858d0e02bac6c0b631
zozo96 <sophie_yy@yeah.net>
2018-04-27 21:10:32 +0800

    最大公约数/两个方阵相乘/数组倒置

--- src/algorithums/algorithm4th/Base1_1/context/Gcd.java
+++ src/algorithums/algorithm4th/Base1_1/context/Gcd.java
@@ -1,5 +1,8 @@
 package algorithm4th.Base1_1.context;
 
+
+import algorithm4th.MyUtils;
+
 /**
  * 最大公约数  欧几里得算法
  *
@@ -14,5 +17,31 @@ public class Gcd {
         if (q == 0) return p;
         int r = p % q;
         return gcd(q, r);
+    }
+}
+
+class Reverse {
+    // 数组元素颠倒
+    public void reverse(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n / 2; i++) {
+            MyUtils.exch(a, i, n - 1 - i);
+        }
+    }
+}
+
+class Matrix {
+    // 两方阵相乘
+    public void TwoMatrix_Multiply(int[][] a, int[][] b) {
+        int n = a.length;
+        double[][] c = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // 计算a.行i和b.列j的点乘
+                for (int k = 0; k < n; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
     }
 }
---
+++ src/algorithums/algorithm4th/MyUtils.java
@@ -0,0 +1,19 @@
+package algorithm4th;
+
+/**
+ * Created by Songyanyan on 18-4-24
+ */
+public class MyUtils {
+    static public int[] exch(int[] a, int behind, int front) {
+        int temp = a[front];
+        a[front] = a[behind];
+        a[behind] = temp;
+        return a;
+    }
+
+    static public boolean less(int n1, int n2) {
+        if (n1 < n2)
+            return true;
+        return false;
+    }
+}
