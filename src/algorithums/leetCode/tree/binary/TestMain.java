4ecaa7c9c284a481495dcae5f07115f0c6c62427
宋燕燕 <songyanyan@hd123.com>
2018-12-13 13:49:22 +0800

    调整提示

--- src/algorithums/leetCode/tree/binary/TestMain.java
+++ src/algorithums/leetCode/tree/binary/TestMain.java
@@ -15,17 +15,15 @@ package leetCode.tree.binary;
 public class TestMain {
   public static void main(String[] args) {
     // 先序中序成树
-    // PreOrderInOrderTraversal p = new PreOrderInOrderTraversal();
-    // TreeNode treeNode = p.buildTree(new int[]{3,9,20,15,7},new int[]{9,3,15,20,7});
+    PreOrderInOrderTraversal p = new PreOrderInOrderTraversal();
+    TreeNode treeNode = p.buildTree(new int[] { 3, 9, 20, 15, 7 }, new int[] { 9, 3, 15, 20, 7 });
     
     // 后序中序成树
-    // PostOrderInOrderTraversal postOrderInOrderTraversal = new PostOrderInOrderTraversal();
-    // TreeNode pTreeNode = postOrderInOrderTraversal.buildTree(new int[] { 9, 3, 15, 20, 7 },
-    // new int[] { 9, 15, 7, 20, 3 });
-    // String str = "1";
-    // System.out.println(pTreeNode);
-    // System.out.println((int) Math.floor(Math.sqrt(5)));
-    // count and say 1 : 1 2:1 1 3: 2 1
+    PostOrderInOrderTraversal postOrderInOrderTraversal = new PostOrderInOrderTraversal();
+    TreeNode pTreeNode = postOrderInOrderTraversal.buildTree(new int[] { 9, 3, 15, 20, 7 },
+      new int[] { 9, 15, 7, 20, 3 });
+    String str = "1";
+    System.out.println(pTreeNode);
+    System.out.println((int) Math.floor(Math.sqrt(5)));
   }
-  
 }
