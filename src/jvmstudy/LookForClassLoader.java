16fddb956e564215550ceb52484a10204b79d776
宋燕燕 <songyanyan@hd123.com>
2019-02-27 22:32:46 +0800

    JVM参数示例

--- src/jvmstudy/LookForClassLoader.java
+++ src/jvmstudy/LookForClassLoader.java
@@ -6,14 +6,27 @@
  * 2019-02-11 - Songyanyan - 创建。
  */
 
+import java.util.Vector;
+
 /**
  * 1.查看系统的类加载器,Java默认提供三个ClassLoader 2.查看内存回收method.showGCInfo
  *
  * @author Songyanyan
  */
 public class LookForClassLoader {
+  private static int count = 0;
   public static void main(String[] args) {
-    showGCInfo();
+    
+    // showGCInfo();
+    // printXmxXms();
+    // testGC();
+    // testHeapDump();
+    try {
+      testStackDeep(0L, 0L, 0L);
+    } catch (Throwable e) {
+      System.out.println("deep of calling:" + count);
+      e.printStackTrace();
+    }
   }
   
   public void findClassLoader() {
@@ -31,5 +44,40 @@ public class LookForClassLoader {
     // -verbose:jni
     LookForClassLoader obj = new LookForClassLoader();
     System.gc();
+  }
+  
+  public static void printXmxXms() {
+    byte[] b = new byte[4 * 1024 * 1024];
+    System.out.println("分配了4M空间给数组");
+    System.gc();
+    System.out.println("回收内存");
+    System.out.print("Xmx=");
+    System.out.println(Runtime.getRuntime().maxMemory() / 1024 / 1024 + "M");
+    System.out.print("free mem=");
+    System.out.println(Runtime.getRuntime().freeMemory() / 1024 / 1024 + "M");
+    System.out.print("total mem=");
+    System.out.println(Runtime.getRuntime().totalMemory() / 1024 / 1024 + "M");
+  }
+  
+  public static void testGC() {
+    // -Xmx20m -Xms20M -Xmn15m -XX:+PrintGCDetails
+    byte[] b = null;
+    for (int i = 0; i < 10; i++)
+      b = new byte[1024 * 1024];
+    
+  }
+  
+  public static void testHeapDump() {
+    Vector vector = new Vector();
+    for (int i = 0; i < 25; i++) {
+      vector.add(new byte[1024 * 1024]);
+    }
+  }
+  
+  // 去掉局部变量 调用层次可以更深
+  public static void testStackDeep(long a, long b, long c) {
+    long e = 1, f = 2, g = 3, h = 4;
+    count++;
+    testStackDeep(a, b, c);
   }
 }
