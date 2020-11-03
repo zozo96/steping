b110f6a2d19f8cdd69e640c12203bff439c063f2
Sonya <sophie_yy@yeah.net>
2020-11-04 01:40:56 +0800

    1. 调整结构 2. 添加websocket服务端demo

---
+++ src/src/main/java/socket/websocket/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package socket.websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author Sonya
+ * @date 2020/11/4 1:09
+ */
+@Component
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
---
+++ src/src/main/java/start/StepStart.java
@@ -0,0 +1,16 @@
+package start;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sonya
+ * @date 2020/11/4 1:20
+ */
+@SpringBootApplication
+public class StepStart {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
